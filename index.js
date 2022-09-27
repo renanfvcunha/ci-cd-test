42f8e5b37bbf14d6a9ae479d19c6350631245624
Renan Cunha <rfvcunha@hotmail.com>
2022-09-26 21:29:55 -0300

    modify app version

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import packageInfo from './package.json' assert { type: 'json' }
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'API Working!', version: packageInfo.version })
+  return res.json({ message: 'API Working! Version 2!', version: packageInfo.version })
 })
 
 app.listen(3000, () => console.log('Listening at 3000'))
